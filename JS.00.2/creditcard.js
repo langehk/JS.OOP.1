5980386630c71e2337a9c49b031a20b5746272b3
langehk <32093132+langehk@users.noreply.github.com>
2020-10-29 15:00:01 +0100

    validate card

--- JS.00.2/creditcard.js
+++ JS.00.2/creditcard.js
@@ -8,30 +8,34 @@ class Creditcard{
 }
 
 function ccNumberValidation(){
-    //let creditcardnumber = document.getElementById('creditcardnumber'); 
-    let creditcardnumber = 123465798;
+    let creditcardnumber = document.getElementById('creditcardnumber').value; 
+    
+    
     let arr = Array.from(creditcardnumber.toString()).map(Number); //Vi sætter kortnummeret ind i et array
     let removedDigit = arr.splice(-1);  //Vi fjerner det sidste nummer
     
-    debugger;
-    let sum; 
+    let sum = 0; 
     arr.reverse(); //Vi vender arrayet om 
     
-    for(let i = 0; i < arr.length; i=+2){ //vi bruger et for-loop til at gange hver anden op
-        arr[i]*2; 
+    for(let i = 0; i < arr.length; i+=2){ //vi bruger et for-loop til at gange hver anden op
+        arr[i] = arr[i]*2; 
 
         if(arr[i] > 9 ){ //hvis det er større end 9, skal der trækkes 9 fra
-            arr[i] - 9;
+            arr[i]= arr[i] - 9;
         }
+        
         
     }
 
     for(let y = 0; y < arr.length; y++){
-        sum =+ arr[y]; //Det nye array lægges sammen
+      
+        sum += arr[y]; //Det nye array lægges sammen
     }
 
-    sum =+ removedDigit; //vi lægger det fjernede nummer til
+    sum += removedDigit[0]; //vi lægger det fjernede nummer til
 
+    console.log(arr, "sum: " + sum);
+debugger;
     if(sum%10 == 0){
         return true; //Kortnummeret er validt
     }
@@ -39,7 +43,7 @@ function ccNumberValidation(){
         return false; 
     }
 
-    console.log(arr, "sum: " + sum);
+    
     
 }
 
